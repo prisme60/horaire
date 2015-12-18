0d55ff2b95b67767e45366f086d08e4579863cf1
Christian FOUCHER <christian.foucher@gmail.com>
2015-12-18 18:18:15 +0100

    Adapt the scrapping to the new transilien website (some work is still needed in order to remove some spaces)

--- scrappingHoraire.py
+++ scrappingHoraire.py
@@ -8,22 +8,23 @@
 
 import lxml.etree
 import lxml.html
+import urllib.request
+import gzip
 
-def selecteurTransilien(root):
+def selecteur_transilien(root:object):
     # Use cssselect to select elements by their css code
-    horaires = root.cssselect(".etat_trafic tr")      # returns 7 elements (1 header and 6 data)
+    horaires = root.cssselect("table.recherche-horaires-resultats tbody tr")      # returns 7 elements (1 header and 6 data)
     fields = {}
 
-    if len(horaires)>=1:
-        del horaires[0]
+    if len(horaires)>0:
+        fields = {"code"       :"td.train span.code",
+                  "heure"      :"td span.hour",
+                  "destination":"td.train + td + td",
+                  "voie"       :"td span.pathway"}
 
-        fields = {"code"       :".nom",
-                  "heure"      :".nom + td",
-                  "destination":".nom + td + td",
-                  "voie"       :".nom + td + td + td + td"}
     return horaires, fields
 
-def selecteurRATP(root):
+def selecteur_RATP(root:object):
     # Use cssselect to select elements by their css code
     horaires = root.cssselect(".rer table tbody tr")      # returns n elements
     fields = {}
@@ -33,13 +34,10 @@ def selecteurRATP(root):
                   "situation"  :".passing_time"}
     return horaires, fields
 
-def horaires_dict(pathInfo):
+def horaires_dict(pathInfo:str):
     """ return extracted times according given pathInfo with dictionary format """
-    (selecteur, url) = urls[pathInfo]
-    # To load directly from a url, use
-    root = lxml.html.parse(url).getroot()
-
-    horaires, fields = selecteur(root)
+    (horaires_fct, url) = urls[pathInfo]
+    horaires, fields = horaires_fct(pathInfo)
 
     extractions = []
     for horaire in horaires:
@@ -52,20 +50,40 @@ def horaires_dict(pathInfo):
             dict_horaire[k] = texte
         extractions.append(dict_horaire)
     return extractions
-
-def horaires(pathInfo):
+    
+def horaires_ratp(url):
     """ return extracted times according given pathInfo with list format (ready to display) """
-    (selecteur, url) = urls[pathInfo]
-
     #print help(lxml.html.parse)
     #print("url=" + url)    
     # To load directly from a url, use
     root = lxml.html.parse(url).getroot()
-
+    
     # Whenever you have an lxml element, you can convert it back to a string like so:
     #print lxml.etree.tostring(root)
 
-    horaires, fields = selecteur(root)
+    return selecteur_RATP(root)
+
+def horaires_transilien(data):
+  req = urllib.request.Request('http://www.transilien.com/horaires/prochains-departs')
+  req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8')
+  req.add_header('Accept-Encoding', 'gzip')
+  #resource = urllib.request.urlopen(req,b'nomGare=PONTOISE')
+  resource = urllib.request.urlopen(req, data)
+  #try:
+  r = gzip.decompress(resource.read())
+  #except OSError:
+  #  pass
+  content = r.decode('utf-8')
+  #print(content)
+  root = lxml.html.document_fromstring(content)
+  return selecteur_transilien(root)
+  
+
+
+def horaires(pathInfo:str):
+    """ return extracted times according given pathInfo with list format (ready to display) """
+    (horaires_fct, data) = urls[pathInfo]
+    horaires, fields = horaires_fct(data)
 
     extractions = []
     for horaire in horaires:
@@ -94,20 +112,42 @@ def horaires(pathInfo):
 def getUrls():
     return urls
 
+
+# POST http://www.transilien.com/horaires/prochains-departs
+# Content-Type: application/x-www-form-urlencoded
+# nomGare=CHARS
+#  -- response --
+# 200 OK
+# Server:  Apache-Coyote/1.1
+# X-Powered-By:  Servlet 2.5; JBoss-5.0/JBossWeb-2.1
+# Content-Type:  text/html;charset=UTF-8
+# Content-Language:  fr-FR
+# Content-Encoding:  gzip
+# Vary:  Accept-Encoding
+# X-Varnish:  131270449, 150663663
+# Transfer-Encoding:  chunked
+# Date:  Wed, 02 Dec 2015 16:20:54 GMT
+# Age:  0
+# Connection:  keep-alive
+# Via:  1.1 varnish, 1.1 bou2-ncdn-middle-http00, 1.1 bou2-ncdn-edge-http00
+
+
+
 urls = {
-"chars"        : (selecteurTransilien,'http://www.transilien.com/gare/CHARS-8738119'),
-"pontoise"     : (selecteurTransilien,'http://www.transilien.com/gare/PONTOISE-8727613'),
-"p"            : (selecteurTransilien,'http://www.transilien.com/gare/pagegare/filterListeTrains?codeTR3A=PSE&destination=&ligne=J&nomGare=PONTOISE&x=46&y=11'),
-"sartrouville" : (selecteurTransilien,'http://www.transilien.com/gare/SARTROUVILLE-8738641'),
-"s"            : (selecteurTransilien,'http://www.transilien.com/gare/pagegare/filterListeTrains?codeTR3A=SVL&destination=CERGY+PREFECTURE&ligne=&nomGare=SARTROUVILLE&x=38&y=11'),
-"psl"          : (selecteurTransilien,'http://www.transilien.com/gare/GARE-DE-PARIS-SAINT-LAZARE-8738400'),
-"psll"         : (selecteurTransilien,'http://www.transilien.com/gare/pagegare/filterListeTrains?codeTR3A=PSL&destination=&ligne=L&nomGare=GARE+DE+PARIS+SAINT-LAZARE&x=26&y=12'),
-"pslj"         : (selecteurTransilien,'http://www.transilien.com/gare/pagegare/filterListeTrains?codeTR3A=PSL&destination=&ligne=J&nomGare=GARE+DE+PARIS+SAINT-LAZARE&x=29&y=6'),
-"auber"        : (selecteurRATP      ,'http://www.ratp.fr/horaires/fr/ratp/rer/prochains_passages/RA/Auber/A'),
-"lepecq"       : (selecteurRATP      ,'http://www.ratp.fr/horaires/fr/ratp/rer/prochains_passages/RA/Le+Vesinet+le+Pecq/R')
+"chars"        : (horaires_transilien,b'nomGare=CHARS'),
+"pontoise"     : (horaires_transilien,b'nomGare=PONTOISE'),
+"p-chars"      : (horaires_transilien,b'nomGare=PONTOISE&destination=CHARS'),
+"sartrouville" : (horaires_transilien,b'nomGare=SARTROUVILLE'),
+"s-cergypref"  : (horaires_transilien,b'nomGare=SARTROUVILLE&destination=CERGY+PREFECTURE'),
+"psl"          : (horaires_transilien,b'nomGare=GARE+DE+PARIS+SAINT-LAZARE'),
+"psl-nu"       : (horaires_transilien,b'nomGare=GARE+DE+PARIS+SAINT-LAZARE&destination=NANTERRE+UNIVERSITE'),
+"psl-pontoise" : (horaires_transilien,b'nomGare=GARE+DE+PARIS+SAINT-LAZARE&destination=PONTOISE'),
+"auber"        : (horaires_ratp      ,'http://www.ratp.fr/horaires/fr/ratp/rer/prochains_passages/RA/Auber/A'),
+"lepecq"       : (horaires_ratp      ,'http://www.ratp.fr/horaires/fr/ratp/rer/prochains_passages/RA/Le+Vesinet+le+Pecq/R')
 }
 
 if __name__ == '__main__':
     for code, (selecteurSite, url) in urls.items():
+        print("--------------" + code + "--------------")
         print(horaires(code))
 
